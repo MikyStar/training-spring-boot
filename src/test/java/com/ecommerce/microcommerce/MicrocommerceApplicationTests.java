a09ac3244f8499554f3907dab2eec8f34105a0a7
AUBENAS Mike <mike.aubenas@gmail.com>
2020-01-06 20:00:36 +0100

    Add test for product ID

--- src/test/java/com/ecommerce/microcommerce/MicrocommerceApplicationTests.java
+++ src/test/java/com/ecommerce/microcommerce/MicrocommerceApplicationTests.java
@@ -8,13 +8,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import java.util.ArrayList;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.ecommerce.microcommerce.dao.ProductDao;
+import com.ecommerce.microcommerce.model.Product;
 import com.ecommerce.microcommerce.web.controller.ProductController;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.is;
@@ -24,16 +30,30 @@ import static org.hamcrest.Matchers.is;
 public class MicrocommerceApplicationTests {
 
 	MockMvc mockMvc;
+
+	@Autowired
 	private WebApplicationContext context;
+	
+	@Autowired
+	ProductDao productDao;
+	
 	ProductController productController;
+	
 	
 	@Before
 	public void setup() throws Exception {
-		this.mockMvc = standaloneSetup(this.productController).build();
+		this.mockMvc = MockMvcBuilders.webAppContextSetup( context ).build();
 	}
 	
 	@Test
-	public void contextLoads() {
+	/** @see https://blog.zenika.com/2016/11/25/spring-mvc-test-dans-un-contexte-securise/ */
+	public void testProductID() throws Exception 
+	{
+		// ArrayList<Product> products = ( ArrayList<Product> ) productDao.findAll();
+		
+		this.mockMvc.perform( get( "/Produits/{id}", 1 ).accept( MediaType.APPLICATION_JSON ) )
+			.andExpect( status().isOk() )
+			.andExpect( jsonPath( "$.nom" ).value( "Ordinateur portable" ) );
 	}
 
 }
