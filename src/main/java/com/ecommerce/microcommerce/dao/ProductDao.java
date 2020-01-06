9d1748b15212efe0c724218edde0e7a9ed934959
Alexandra <alexandra-robin@outlook.com>
2020-01-06 18:51:29 +0100

    Add Api Documentation

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -1,6 +1,10 @@
 package com.ecommerce.microcommerce.dao;
 
 import com.ecommerce.microcommerce.model.Product;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,20 +13,27 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@Api(description="Interface entre les objets Java et la base de données")
 public interface ProductDao extends JpaRepository<Product, Integer> {
 
+    @ApiOperation(value = "Récupère un produit par son ID passé en paramètre")
     Product findById(int id);
 
+    @ApiOperation(value = "Récupère une liste de produits dont le prix est strictement supérieur au prix passé en paramètre")
     List<Product> findByPrixGreaterThan(int prixLimit);
 
-    List<Product> findByNomLike(String recherche);
+    @ApiOperation(value = "Récupère une liste de produits dont le nom match la chaîne passée en paramètre")
+	List<Product> findByNomLike(String recherche);
 
+    @ApiOperation(value = "Récupère la liste des produits (ID, nom, prix) dont le prix est strictement supérieur au prix passé en paramètre")
     @Query("SELECT id, nom, prix FROM Product p WHERE p.prix > :prixLimit")
     List<Product>  chercherUnProduitCher(@Param("prixLimit") int prix);
     
+    @ApiOperation(value = "Récupère la liste des produits (ID, nom, prix) par ordre alphabétique")
     @Query("SELECT id, nom, prix FROM Product p ORDER BY p.nom")
     List<Product>  trierProduitsParOrdreAlphabetiqueAdmin();
 
+    @ApiOperation(value = "Récupère la liste des produits (ID, nom) par ordre alphabétique")
     @Query("SELECT id, nom FROM Product p ORDER BY p.nom")
     List<Product>  trierProduitsParOrdreAlphabetique();
     
--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitIntrouvableException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitIntrouvableException.java
@@ -3,7 +3,10 @@ package com.ecommerce.microcommerce.web.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import io.swagger.annotations.Api;
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
+@Api(description="Exception levée lorsque le produit n'est pas trouvé")
 public class ProduitIntrouvableException extends RuntimeException {
 
     public ProduitIntrouvableException(String s) {
